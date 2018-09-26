e65e73a8c221d7acb8550d83554e9b54172123e7
djstephenson7 <dan_0502@hotmail.co.uk>
2018-09-26 12:02:28 +0100

    Player can add name

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -4,7 +4,8 @@ describe Quiz do
   feature 'test' do
     scenario 'testing' do
       visit '/'
-      expect(page).to have_content('Testing infrastructure working!')
+      expect(page).to have_content('Welcome! Please enter your name!')
+      fill_in :player_name, with: "Daniel"
     end
   end
 end
