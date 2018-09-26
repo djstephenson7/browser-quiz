5815f816047e878469d202ce42a5f9bc11cba01a
djstephenson7 <dan_0502@hotmail.co.uk>
2018-09-26 12:43:53 +0100

    Added player sign-in

--- app.rb
+++ app.rb
@@ -1,7 +1,19 @@
 require 'sinatra/base'
 
 class Quiz < Sinatra::Base
+  enable :sessions
+  
   get '/' do
     erb :index
+  end
+
+  post '/name' do
+    session[:player_name] = params[:player_name]
+    redirect '/quiz'
+  end
+
+  get '/quiz' do
+    @name = session[:player_name]
+    erb :quiz
   end
 end
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -1,11 +1,23 @@
 require './app'
 
 describe Quiz do
-  feature 'test' do
-    scenario 'testing' do
+  feature 'Enter player name' do
+    scenario 'Enters player name' do
       visit '/'
       expect(page).to have_content('Welcome! Please enter your name!')
       fill_in :player_name, with: "Daniel"
+      click_button ('Submit')
+    end
+  end
+
+  feature 'Greets player' do
+    scenario 'Hi @name!' do
+      visit '/'
+      expect(page).to have_content('Welcome! Please enter your name!')
+      fill_in :player_name, with: "Daniel"
+      click_button 'Submit'
+      visit '/quiz'
+      expect(page).to have_content 'Welcome, Daniel'
     end
   end
 end
